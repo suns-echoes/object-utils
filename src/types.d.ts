f139c4da8bc00da259477875f4dcf6c0e9e63547
Aneta Suns <suns.echoes@gmail.com>
2022-02-02 20:05:06 +0100

    Update types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,8 +1,20 @@
 type PathKeys = string[];
 type KeyPath = string;
 
-type AnyArray = any[];
+declare type AnyKey = number | string | symbol;
 
-type AnyObject = Record<string, any>;
+declare type ObjectKey = string | symbol;
 
-type AnyFunction = (...args: any[]) => any;
+declare interface AnyArray extends Array<any> {}
+
+declare interface AnyClass extends AnyObject {
+	new (...args: any[]): any,
+}
+
+declare interface AnyFunction extends AnyObject {
+	(...args: any[]): any,
+}
+
+declare interface AnyObject extends Record<ObjectKey, any> {}
+
+declare interface ObjectLike extends Record<AnyKey, any> {}
